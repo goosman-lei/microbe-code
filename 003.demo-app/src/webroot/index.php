a8e68e82d10af0f59f51af21eabe8aa9fbaf73b1
goosman.lei <goosman.lei@gmail.com>
2020-05-26 15:11:26 +0800

    change to runner/webpage

--- 003.demo-app/src/webroot/index.php
+++ 003.demo-app/src/webroot/index.php
@@ -2,8 +2,8 @@
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
 $config = new \Microbe\Config\File(__DIR__ . '/../../src/conf');
-\Microbe\Cgi\Runner::getInstance()->init($config);
+\Microbe\Runner\Webpage::getInstance()->init($config);
 
 \Microbe\Microbe::$ins->appendChain(new \MicrobeDemo\Action());
 
-\Microbe\Cgi\Runner::getInstance()->run();
+\Microbe\Runner\Webpage::getInstance()->run();

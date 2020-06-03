2e7a3b7b107bef79d7739c93bbd54f3546129dac
goosman.lei <goosman.lei@gmail.com>
2020-06-03 18:25:54 +0800

    add env

--- 003.demo-app/src/Daemon/Welcome/Sayhi.php
+++ 003.demo-app/src/Daemon/Welcome/Sayhi.php
@@ -2,6 +2,7 @@
 namespace MicrobeDemo\Daemon\Welcome;
 class Sayhi extends \Microbe\Scene\Daemon\Action {
     public function exec() {
+        $this->response->print("ENV_A: " . $this->request->getEnv('ENV_A') . "\n");
         $this->response->printf("Options:\n%s\n\n", print_r($this->request->getOptions(), TRUE));
         $this->response->printf("Args:\n%s\n\n", print_r($this->request->getArgs(), TRUE));
     }

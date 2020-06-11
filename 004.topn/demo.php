1eee29669af439755194a9e89ebeda73b461cd03
goosman.lei <goosman.lei@gmail.com>
2020-06-11 11:23:12 +0800

    redo

--- 004.topn/demo.php
+++ 004.topn/demo.php
@@ -13,61 +13,50 @@ function dump($datas) {
     echo chr(10);
 }
 
-/*
-N: 数字总数
-M: 要找出最大的M个数
-
-Version1: (N - M) * 2 * M
-Version2: (N - M) * 1 ---------- (N - M) * M
-*/
 class TopNFilter {
+    protected $datas = [];
     protected $capacity;
-    protected $datas;
-    protected $min_index = -1;
-
+    protected $minIndex = -1;
     public function __construct($capacity) {
-        if ($capacity < 1) {
-            throw new Exception('Invalid Capacity');
-        }
         $this->capacity = $capacity;
-        $this->datas    = [];
     }
     public function push($num) {
         if (count($this->datas) < $this->capacity) {
             array_push($this->datas, $num);
-        } else if ($num > $this->min_num()) {
-            $this->replace_min($num);
-        }
-    }
-    protected function min_num() {
-        return $this->datas[$this->min_index()];
-    }
-    protected function replace_min($num) {
-        $this->datas[$this->min_index()] = $num;
-        $this->min_index = -1;
-    }
-    protected function min_index() {
-        if ($this->min_index >= 0) {
-            return $this->min_index;
-        }
-        $this->min_index = 0;
-        foreach ($this->datas as $index => $num) {
-            if ($num < $this->datas[$this->min_index]) {
-                $this->min_index = $index;
+        } else {
+            $minIndex = $this->minIndex();
+            if ($num > $this->datas[$minIndex]) {
+                $this->datas[$minIndex] = $num;
+                $this->minIndex = -1;
             }
         }
-        return $this->min_index;
     }
     public function datas() {
         return $this->datas;
+    }
+    protected function minIndex() {
+        if ($this->minIndex != -1) {
+            return $this->minIndex;
+        }
+        $minIndex = 0;
+        foreach ($this->datas as $index => $num) {
+            if ($num < $this->datas[$minIndex]) {
+                $minIndex = $index;
+            }
+        }
+        $this->minIndex = $minIndex;
+        return $minIndex;
     }
 }
 
 $filter = new TopNFilter(10);
 $datas = gen_nums(20, 1, 99);
 dump($datas);
-rsort($datas, SORT_NUMERIC);
-dump($datas);
+
+$rdatas = $datas;
+rsort($rdatas, SORT_NUMERIC);
+dump($rdatas);
+
 foreach ($datas as $num) {
     $filter->push($num);
 }
